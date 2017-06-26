98922442ea373c0e6c0d1ff151ddae96f8f79e72
Evelyn <carina120794@gmail.com>
2017-06-26 11:42:33 -0500

    Arreglosyobjetos

--- ejercicios.js
+++ ejercicios.js
@@ -1,4 +1,64 @@
-//Primer ejercicio
+
+//ejercicio 1
+function multiplo (a,divisor){
+var array_nuevo=[];
+a.forEach(function(a){
+  if(a % divisor==0){
+    array_nuevo.push(a);
+  }
+});
+  return array_nuevo;
+
+}
+
+//ejercicio 3
+function multi(array,num){
+ var m=array.reduce(function(valorAnterior, valorActual){
+ return valorAnterior * valorActual;
+});
+  return  multi_total=m*num;
+
+}
+multi([2,2,3],3);
+//ejercicio 4
+
+function numero_doble(numbers){
+return numbers.map(function(x) {
+   return (x * 2)+1;
+});
+}
+
+//ejercicio 5
+function con_temperaturas(arr_celsius){
+ return arr_celsius.map(function(c){
+   return (9/5 *c)+32;
+ });
+}
+//ejercicio 6
+
+function acumulador(array){
+ return array.reduce(function(valorAnterior, valorActual){
+ return valorAnterior * valorActual;
+});
+}
+multi([2,2,3]);
+//ejercicio 7
+
+ function edades(edad){
+  var today = new Date();
+  var year = today.getFullYear();
+  var año= edad.map(function(x){
+    if(x%2==0 && x>18){
+   return año_nacimiento=year-x;
+    }
+
+  });
+  document.write(año);
+}
+edades([16,88,23,5]);
+
+
+//Primer ejercicio de objetos
 function Area(x,y){
   this.x=x;
   this.y=y;
@@ -6,7 +66,7 @@ function Area(x,y){
 }
 
 var a_rec=new Area(30,89);
-// segundo ejercicio
+// segundo ejercicio de objetos
 function Persona (nombre,dia,mes,anio){
   this.nombre=nombre;
   this.dia=dia;
